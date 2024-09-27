62821b034692aa640502d68b035b3ae4099f36fc
mugambi12 <silasmungiria16@gmail.com>
2024-09-28 00:42:59 +0300

    Refactor mobile menu to toggle button icon based on menu state

--- public/javascripts/mobileMenu.js
+++ public/javascripts/mobileMenu.js
@@ -3,4 +3,10 @@ const navLinks = document.querySelector(".nav-links");
 
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
+  // Change the button icon based on the menu state
+  if (navLinks.classList.contains("active")) {
+    menuToggle.innerHTML = "&times;"; // Close icon
+  } else {
+    menuToggle.innerHTML = "&#9776;"; // Hamburger icon
+  }
 });
