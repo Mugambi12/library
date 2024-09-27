bd218b7b0e60012af0324791519bd16fe05c876e
mugambi12 <silasmungiria16@gmail.com>
2024-09-28 00:06:40 +0300

    Refactor responsive styles in form, author, book, buttons, and header views

--- server.js
+++ server.js
@@ -31,6 +31,6 @@ app.use("/", indexRouter);
 app.use("/authors", authorsRouter);
 app.use("/books", booksRouter);
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 3000, "0.0.0.0", () => {
   console.log("Server is running on port 3000");
 });
